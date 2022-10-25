6bdc08859edc52b4a66d2d212dfa211cb5a40cb1
Sevalas <se.valas@outlook.com>
2022-10-25 02:17:53 -0300

    first simple server implementation
    Create express server
    
    develop

---
+++ backend/dummy/data.js
@@ -0,0 +1,83 @@
+const data = {
+    products: [
+        {
+            name: 'Nike air force one',
+            slug: 'nike-air-force-one-shoes',
+            category: 'Shoes',
+            image: '/dummy/images/airForceOne.jpg', // 680 x 830
+            price: 300,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Shoes for Swag'
+        },
+        {
+            name: 'Anime Shirt Siyah',
+            slug: 'anime-shirt-siyah',
+            category: 'shirts',
+            image: '/dummy/images/animeShirt.jpg', // 680 x 830
+            price: 80,
+            brand: 'Siyah',
+            rating: 5,
+            numReviews: 14,
+            description: 'UwU'
+        },
+        {
+            name: 'Anime Bucket Hat Akatsuki',
+            slug: 'Anime-bucket-hat-akatsuki',
+            category: 'Hats',
+            image: '/dummy/images/animeBucketHat.jpg', // 680 x 830
+            price: 50,
+            brand: 'Anime',
+            rating: 4,
+            numReviews: 8,
+            description: 'Naruto hat'
+        },
+        {
+            name: 'Black Slim Jeans Levis',
+            slug: 'Black-slim-jeans-levis',
+            category: 'Jeans',
+            image: '/dummy/images/blackJeansLevis.jpg', // 680 x 830
+            price: 250,
+            brand: 'Levis',
+            rating: 5,
+            numReviews: 15,
+            description: 'Black soul'
+        },
+        {
+            name: 'Anime Shirt Siyah',
+            slug: 'anime-shirt-siyah2',
+            category: 'shirts',
+            image: '/dummy/images/animeShirt.jpg', // 680 x 830
+            price: 80,
+            brand: 'Siyah',
+            rating: 5,
+            numReviews: 14,
+            description: 'UwU'
+        },
+        {
+            name: 'Anime Bucket Hat Akatsuki',
+            slug: 'Anime-bucket-hat-akatsuki2',
+            category: 'Hats',
+            image: '/dummy/images/animeBucketHat.jpg', // 680 x 830
+            price: 50,
+            brand: 'Anime',
+            rating: 4,
+            numReviews: 8,
+            description: 'Naruto hat'
+        },
+        {
+            name: 'Black Slim Jeans Levis',
+            slug: 'Black-slim-jeans-levis2',
+            category: 'Jeans',
+            image: '/dummy/images/blackJeansLevis.jpg', // 680 x 830
+            price: 250,
+            brand: 'Levis',
+            rating: 5,
+            numReviews: 15,
+            description: 'Black soul'
+        },
+    ]
+};
+
+export default data;
